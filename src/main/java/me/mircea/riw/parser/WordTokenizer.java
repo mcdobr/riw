52a98a67e4f8a56eefce127c068df24e7cc376ac
mcdobr <mircea.dobreanu@gmail.com>
2019-03-05 13:37:55 +0200

    working first two labs

--- src/main/java/me/mircea/riw/parser/WordTokenizer.java
+++ src/main/java/me/mircea/riw/parser/WordTokenizer.java
@@ -5,25 +5,30 @@ import java.util.Map;
 
 public class WordTokenizer {
     public Map<String, Integer> countOccurences(String str) {
-        Map<String, Integer> occurences = new HashMap<>();
+        final int SINGLE_QUOTE_CODE_POINT = 0x27;
+    	
+    	Map<String, Integer> occurences = new HashMap<>();
 
         StringBuilder builder = new StringBuilder();
-        String delimiters = " \r\n\t\",:;.!?[]{}()<>";
-
-        for (int i = 0; i < str.length(); ++i) {
-            char c = str.charAt(i);
-            if (delimiters.indexOf(c) == -1) {
-                builder.append(c);
-            } else if (builder.length() != 0) {
-                String word = builder.toString();
-                occurences.merge(word, 1, Integer::sum);
-
-                builder.setLength(0);
-            } else {
-                builder.setLength(0);
-            }
+        
+        final int length = str.length();
+        for (int offset = 0; offset < length; ) {
+        	final int codePoint = Character.toLowerCase(str.codePointAt(offset));
+        	
+        	if (Character.isLetterOrDigit(codePoint) || codePoint == SINGLE_QUOTE_CODE_POINT) {
+        		builder.appendCodePoint(codePoint);
+        	} else {
+        		String word = builder.toString();
+        		
+        		if (word.length() > 0)
+        			occurences.merge(word, 1, Integer::sum);
+        		
+        		builder.setLength(0);
+        	}
+        	
+        	offset += Character.charCount(codePoint);
         }
-
+        
         return occurences;
     }
 }
